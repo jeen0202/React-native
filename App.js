ea20e6345b9c6e9036198657327cd59070b4582b
SJ_Win <jeen0202@korea.ac.kr>
2021-05-13 15:33:25 +0900

    Image 추가 실습

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>앱 작성 테스트</Text>
-      <StatusBar style="auto" />
+      
+      
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
+      <Text style={{color: '#888', fontSize: 18}}>
+      To share a Photo from your phone with a friend, just press the button below!
+      </Text>
     </View>
   );
 }
---
+++ Readme.md
@@ -0,0 +1,4 @@
+# React Native
+## React Native란?
+Facebook에서 만든 Open Source moblie App framework
+React를 사용하여 Android,ios,Web 및 UWP 용 APP을 개발하는데 사용된다.
