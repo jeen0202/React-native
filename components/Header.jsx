16d375d0bfcf0c589cad999ee1b963007cc1af67
sejing <jeen0202@korea.ac.kr>
2021-05-15 22:16:03 +0900

    component 연동을 위한 button 추가

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {View,Text,StyleSheet} from 'react-native'
+import {View,Text,StyleSheet, TouchableOpacity} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
 
 const Header = () =>{
     return(
@@ -7,12 +8,21 @@ const Header = () =>{
             <Text style={styles.title}>
                 할 일 목록
             </Text>
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style = {styles.button}>
+                    <Ionicons name ='ios-add' color='#FFFFFF' size={24}/>
+                </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        //각 요소가 양쪽으로 떨어지게 만든다.
         marginTop : 56,
         marginBottom : 16,
         marginLeft : 16,
@@ -21,6 +31,22 @@ const styles = StyleSheet.create({
         color : '#212121',
         fontSize : 32,
         fontWeight : '600',
+    },
+    button: {
+        width: 28,
+        height : 28,
+        borderRadius:14,
+        backgroundColor: '#212121',
+        justifyContent: 'center',
+        alignItems:'center',
+        paddingTop : Platform.select({
+            ios:2,
+            android:0
+        }),
+        paddingLeft: Platform.select({
+            ios :1,
+            android :0,
+        })
     }
 })
 export default Header
