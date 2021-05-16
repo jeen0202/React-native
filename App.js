91facb712b39d06ec00b45501056d8ed6ddb7ae0
sejing <jeen0202@korea.ac.kr>
2021-05-16 21:53:41 +0900

    추가/삭제 기능을 위한 state 변경 함수 설정

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet,FlatList,SafeAreaView } from 'react-native';
+import {StyleSheet,FlatList,SafeAreaView, TouchableHighlightBase } from 'react-native';
 import Header from './components/Header'
 import TodoItem from './components/TodoItem'
 import TaskModal from './components/TaskModal'
@@ -7,7 +7,7 @@ import TaskModal from './components/TaskModal'
 export default class App extends React.Component{
   state = {
     todos :[
-      {
+      {        
         title : "주간회의",
         done : true,
       },
@@ -15,18 +15,22 @@ export default class App extends React.Component{
         title : "면직처리",
         done : false,
       },
-    ]
+    ],
+    showModal : false
   }
   render(){
     return (
       //SafeAreaView : 앱화면에서 안전하게 노출되는 지역설정
       <SafeAreaView style={styles.container}>
-        <Header />    
+        <Header
+          show={() => {
+            this.setState({ showModal: true})
+          }} />    
         <FlatList
           data = {this.state.todos}
           renderItem={({item}) => {
           return(
-          <TodoItem 
+          <TodoItem          
           title = {item.title} 
           done = {item.done}
           keyExtractor={(_, index) => {
@@ -36,7 +40,10 @@ export default class App extends React.Component{
           )
         }}
         />
-        <TaskModal isVisible={false} />   
+        <TaskModal isVisible={this.state.showModal}
+          hide = {() => {
+            this.setState({ showModal : false})
+          }} />   
       </SafeAreaView>
     );
   }
