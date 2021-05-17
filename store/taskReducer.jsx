57a28c400acf7f4c0094bd2013ddc14d20f4bd0a
SJ_Win <jeen0202@korea.ac.kr>
2021-05-17 16:08:31 +0900

    Reducer 정의

---
+++ store/taskReducer.jsx
@@ -0,0 +1,45 @@
+import { ADD_TASK, DELETE_TASK, DID_TASK } from "./taskTypes";
+
+const initState = {
+    todos : [
+        {"task": "주간회의", "done": true, "id":"1"},
+        {"task": "메일확인", "done": false, "id":"2"},
+        {"task": "근무일지작성", "done" : false, "id":"3"}
+    ]
+}
+
+const taskReducer = (state=initState, action) => {
+    switch (action.type) {
+        case ADD_TASK:
+            return{
+                ...state,
+                todos:[...state.todos, {
+                    task:action.payload, done:false, id: Math.random().toString()
+                }
+            ]
+            }
+        case DELETE_TASK:
+            return {
+                ...state,
+                todos: state.todos.filter(item=> item.id != action.payload)
+            }
+        case DID_TASK:
+            return{
+                ...state,
+                todos: state.todos.map((item) => {
+                    if (item.id != action.payload){
+                        return item
+                    }
+                        return{
+                            ...item,
+                            done:true
+                        }                    
+                })
+            }
+            break;
+        default:
+            return state;
+    }
+}
+
+export default taskReducer
