8adcb3a05a3567c2b9285c1005d0829c12d5e55b
SJ_Win <jeen0202@korea.ac.kr>
2021-05-20 09:53:09 +0900

    AsyncStorage를 활용하여 저장 성공

--- store/persistReducer.jsx
+++ store/persistReducer.jsx
@@ -1,10 +1,10 @@
 import {persistReducer} from 'redux-persist'
-import storage from "redux-persist/lib/storage"
+//import storage from "redux-persist/lib/storage"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import taskReducer from "./taskReducer"
 
 const persistConfig = {
     key: "root",
-    storage,
-    whitelist: ["taskReducer"]
+    storage : AsyncStorage,    
 }
 export default persistReducer(persistConfig,taskReducer);
