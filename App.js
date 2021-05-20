f55144362c2a1a2ace089afb2f26ad1c77a5d89c
SJ_Win <jeen0202@korea.ac.kr>
2021-05-20 09:42:04 +0900

    redux-persist 1차 적용(에러)

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet,FlatList,SafeAreaView } from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {StyleSheet,SafeAreaView } from 'react-native'
+import {persistStore} from "redux-persist"
+import {PersistGate} from "redux-persist/integration/react"
 import Header from './containers/Header'
 import TodoLists from './containers/TodoLists'
 import TaskModal from './containers/TaskModal'
@@ -8,31 +9,23 @@ import TaskModal from './containers/TaskModal'
 //redux를 사용하기 위한 component import
 import store from './store/store'
 import { Provider} from 'react-redux'
-
-
+//redux persist 사용
+const persistor = persistStore(store);
 export default class App extends React.Component{
   //App 실행시 정보 호출  
-  componentDidMount() {
-    AsyncStorage.getItem('@todo:state').then((state)=>{
-      if(state != null){
-        this.setState(JSON.parse(state));
-      }
-    });
 
-  }
-  saveItem =() => {
-    AsyncStorage.setItem('@todo:state',JSON.stringify(this.state))
-  }
   render(){
     return (
       //Provider : Redux 사용을 위한 Component      
       //SafeAreaView : 앱화면에서 안전하게 노출되는 지역설정
       <Provider store = {store}>
-        <SafeAreaView style={styles.container}>
-          <Header/>    
-          <TodoLists/>
-          <TaskModal/>   
-        </SafeAreaView>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaView style={styles.container}>
+            <Header/>    
+            <TodoLists/>
+            <TaskModal/>   
+          </SafeAreaView>
+        </PersistGate>
       </Provider>
     );
   }  
